318108400af347e8672c777eeeeb3a22b2d1cc08
Korkemochka <korakarabzz@gmail.com>
2026-01-22 21:21:03 +0300

    added logic

--- practice.py
+++ practice.py
@@ -1,2 +1,9 @@
-name = input()
-print(f"Hello, {name}")
+import getpass
+
+input_name = input("Enter your name: ")
+os_user = getpass.getuser()
+
+if input_name == os_user:
+    print(f"Hello, {input_name}")
+else:
+    print("LIER")
